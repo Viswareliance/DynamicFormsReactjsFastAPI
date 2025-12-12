582c64060e4c234ae57643a26ac349049aefee45
Viswareliance <viswa.reliancemobile@gmail.com>
2025-12-12 10:27:02 +0530

    dynamicform1 App.jsx file

---
+++ DynamicForm1/client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutPage from "./pages/LayoutPage";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#F8F8F8]">
+      <LayoutPage />
+    </div>
+  );
+};
+
+export default App;
